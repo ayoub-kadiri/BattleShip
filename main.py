82e3679c8f4cb9c690b2599fa49ecbc9eae16dd7
Ayoubsab <85080980+Ayoubsab@users.noreply.github.com>
2021-05-31 23:30:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ fenetre = pygame.display.set_mode( (1200,600) )
 pygame.display.set_caption("BattleShip Game")
 run = True
 clock = pygame.time.Clock()
-posLettre = (20,55)
-
+lettres_position ={chr(i):(20, 55+50*(i-65)) for i in range(65,75)} 
+arialfont = pygame.font.SysFont("arial",40)
 while run:
     clock.tick(50)
     for event in pygame.event.get():
@@ -20,11 +20,11 @@ while run:
       
         pygame.draw.line(fenetre, (127,255,0), (660+i*50,53), (660+i*50,556),4)
         pygame.draw.line(fenetre, (127,255,0), (660,54+i*50), (1160,54+i*50),4)
-      
-    for n in range(65,75):
-        arialfont = pygame.font.SysFont("arial",40)
-        txt = arialfont.render(chr(n),True,(127,255,0))
-        posLettre = (posLettre[0],posLettre[1]+1*(n-65))
-        fenetre.blit(txt,posLettre)
-        pygame.display.flip()
+
+    
+    
+    for lettre in lettres_position.keys():
+        l = arialfont.render(lettre,False,(127,255,0))
+        fenetre.blit(l,lettres_position[l])
+    pygame.display.flip()
 pygame.quit()
