0d2bedc746831482ae926289a473e30107c97ca1
Ayoubsab <85080980+Ayoubsab@users.noreply.github.com>
2021-06-02 23:08:46 +0100

    Update ship.py

--- ship.py
+++ ship.py
@@ -1,16 +1,16 @@
 import pygame
 class Ship:
-    sousMarin_image = pygame.image.load('images/torpilleurImage.png')
-    sousMarin_image = pygame.transform.scale(sousMarin_image, (sousMarin_image.get_width(), 100))
 
-    def __init__(self, name, size, position, orientation, hits): #position = liste de coordonnées
+
+
+    def __init__(self, image, size, position, orientation, hits): #position = liste de coordonnées
         self.size = size
         self.position = position
-        self.name = name
+        self.image = image
         self.orientation = orientation
         self.hits = hits
 
-    hits = [False for i in range(size)]
+        hits = [False for i in range(size)]
 
         
     
