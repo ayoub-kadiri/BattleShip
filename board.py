bccb77c16829d7161a200da19d4023868024f6af
Ayoubsab <85080980+Ayoubsab@users.noreply.github.com>
2021-06-02 23:09:36 +0100

    Update board.py

--- board.py
+++ board.py
@@ -7,23 +7,24 @@ class Board :
     chiffresImages = pygame.image.load('images/chiffres.png')
     chiffresImages = pygame.transform.scale(chiffresImages, (500, 46))
     
-    def __init__(self, xStartPos, cases):
+    def __init__(self, xStartPos, cases, fenetre):
     
         self.xStartPos = xStartPos
         self.cases = cases
+        self.fenetre = fenetre
         for i in range(10):
             for j in range(1,11):
-                cases[self.lettre[i]+str(j)] = [pygame.Rect((xStartPos+4+(j-1)*60, 53+4+i*60), (xStartPos+4+(j-1)*60+60,53+4+i*60+60 )), False]
+                cases[self.lettre[i]+str(j)] = [pygame.Rect((xStartPos+(j-1)*50, 53+i*50), (xStartPos+(j-1)*60+60,53+4+i*60+60 )), False, 'vide']
         
     
     def update(self, case):
         cases[case][1] = True
 
 
-    def drawBoard(self, fenetre):
+    def drawBoard(self):
         for i in range(11):
-            pygame.draw.line(fenetre, (127,255,0), (self.xStartPos+i*50,53), (self.xStartPos+i*50,556),4)
-            pygame.draw.line(fenetre, (127,255,0), (self.xStartPos,54+i*50), (500+self.xStartPos,54+i*50),4)
+            pygame.draw.line(self.fenetre, (127,255,0), (self.xStartPos+i*50,53), (self.xStartPos+i*50,556),4)
+            pygame.draw.line(self.fenetre, (127,255,0), (self.xStartPos,54+i*50), (500+self.xStartPos,54+i*50),4)
         
-        fenetre.blit(self.lettreImage, (self.xStartPos-48,57))
-        fenetre.blit(self.chiffresImages,(self.xStartPos,560))
+        self.fenetre.blit(self.lettreImage, (self.xStartPos-48,57))
+        self.fenetre.blit(self.chiffresImages,(self.xStartPos,560))
